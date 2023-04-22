b4ecbcea3c3fefba4e2c9f6105f9ede0e49609da
Rasna Shakya <rasnashakya@gmail.com>
2023-04-22 16:08:57 +0545

    feat: Added Auth for user

---
+++ lib/dtt_recharger_web/controllers/user_registration_html.ex
@@ -0,0 +1,5 @@
+defmodule DttRechargerWeb.UserRegistrationHTML do
+  use DttRechargerWeb, :html
+
+  embed_templates "user_registration_html/*"
+end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,12 +1,20 @@
 # Insert vibes
 
 alias DttRecharger.Repo
-alias DttRecharger.Schema.FileType
+alias DttRecharger.Schema.{FileType, Role}
 
 file_types = ["stock", "order"]
 
 Enum.each(file_types, fn(file_type) ->
   %FileType{}
   |> FileType.changeset(%{type: file_type})
+  |> Repo.insert
+end)
+
+roles = ["superadmin", "admin", "uploader", "authorizer"]
+
+Enum.each(roles, fn(role) ->
+  %Role{}
+  |> Role.changeset(%{name: role})
   |> Repo.insert
 end)
